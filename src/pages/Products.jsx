4332e2530571ef34e845d9878ed7f1ed49b3b5a4
JAYANTI THAKOR <jyantithakor941@gmail.com>
2026-03-19 15:30:54 +0530

    search and filter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,15 +7,35 @@ const Products = () => {
     return allProducts ? JSON.parse(allProducts) : [];
   });
 
+
+  const [search,setSearch] = useState("");
+  const [category,setCategory] = useState("all");
+
+  const filterProducts = products.filter((p)=>{
+    const matchSearch = p.name.toLowerCase().includes(search.toLowerCase());
+    const matchCategory = category === 'all'?true:p.category.toLowerCase() == category.toLowerCase();
+    return matchSearch && matchCategory;
+  })
+
   return (
     <div>
       <div className="hero-title">
          <h1>Upgrade Your Home with Premium Furniture</h1>
          <p>Shop high-quality furniture at unbeatable prices. From sofas to dining sets, find everything you need to create your dream home.</p>
       </div>
+      <div className="search-container">
+        <input type="text" placeholder='Search Products' value={search} onChange={(e)=>setSearch(e.target.value)} />
+        <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+          <option value="all">All</option>
+          <option value="living room">Living Room</option>
+          <option value="dining">Dining</option>
+          <option value="bedroom">Bedroom</option>
+          <option value="study">Study</option>
+        </select>
+      </div>
       <div className="product-container">
         {
-          products.map((p)=>(
+          filterProducts.map((p)=>(
             <div className="card">
               <img src={p.image} alt="" />
               <div className="product-info">
